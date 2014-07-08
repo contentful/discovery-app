dc32502e3311cf5ce1e91b562d67f35364920817
Boris Bügling <boris@icculus.org>
2014-07-08 11:28:38 +0200

    Fix sizing issues of web views.

--- Code/CDAWebController.m
+++ Code/CDAWebController.m
@@ -9,6 +9,15 @@
 #import "CDAWebController.h"
 #import "UIView+Geometry.h"
 
+@interface CDAWebController ()
+
+@property CGFloat originalHeight;
+@property UIView* overlayView;
+
+@end
+
+#pragma mark -
+
 @implementation CDAWebController
 
 -(id)initWithURL:(NSURL *)url {
@@ -23,12 +32,30 @@
     return self;
 }
 
+-(void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.overlayView = [[UIView alloc] initWithFrame:self.view.bounds];
+    self.overlayView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.overlayView];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    self.originalHeight = self.view.height;
+}
+
 #pragma UIWebViewDelegate
 
 -(void)webViewDidFinishLoad:(UIWebView *)_webView {
     [super webViewDidFinishLoad:_webView];
 
+    [self.overlayView removeFromSuperview];
+    self.overlayView = nil;
+
     self.view.y = 20.0;
+    self.view.height = self.originalHeight + 40.0;
 }
 
 @end
